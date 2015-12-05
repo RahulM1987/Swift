c6f5ba4b2dae3b51d2bf61baed74d52495cadb04
RahulM1987 <Apple@rahuls-macbook-pro-2.local>
2015-12-05 23:08:48 +0530

    Initial Commit

---
+++ Swift2Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift2Test
+//
+//  Created by Rahul's MacBook Pro on 05/12/15.
+//  Copyright © 2015 Rahul M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
